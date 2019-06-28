acab6199e9062f17737645c172488dc6223415df
Chibueze Ejiofor <chibuezeejiofor@fb.com>
2019-06-28 09:18:16 -0700

    Done with required functionality

---
+++ app/src/main/java/com/example/flickster/models/Config.java
@@ -0,0 +1,33 @@
+package com.example.flickster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Config {
+    String imageBaseUrl;
+    // the poster size to use when fetching images, part of the url
+    String posterSize;
+
+    public Config(JSONObject object) throws JSONException {
+        this.imageBaseUrl = imageBaseUrl;
+        JSONObject images = object.getJSONObject("images");
+        imageBaseUrl = images.getString("secure_base_url");
+        // gets the poster size
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
+        // use the option at index 3 or w342 as a fallback
+        posterSize = posterSizeOptions.optString(3,"w342");
+
+    }
+    public String getImageUrl(String size, String path){
+        return String.format("%s%s%s", imageBaseUrl, size, path);
+    }
+
+    public String getPosterSize() {
+        return posterSize;
+    }
+
+    public String getImageBaseUrl() {
+        return imageBaseUrl;
+    }
+}
---
+++ app/src/main/java/com/example/flickster/models/MovieAdapter.java
@@ -0,0 +1,83 @@
+package com.example.flickster.models;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flickster.R;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    ArrayList<Movie> movies;
+    Config config;
+    Context context;
+
+    public MovieAdapter(ArrayList<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public Config getConfig() {
+        return config;
+    }
+
+    public void setConfig(Config config) {
+        this.config = config;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // get the context and create the inflater
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        // create the view using the item_movie layout
+        View movieView = inflater.inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+    // binds an inflated view to a new item
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Movie movie = movies.get(position);
+        //holder.ivPosterImage.setImageIcon(movie.getPosterPath());
+        holder.tvTitle.setText(movie.getTitle());
+        holder.tvOverview.setText(movie.getOverview());
+        String imageUrl = config.getImageUrl(config.getPosterSize(), movie.getPosterPath());
+        //load image using guide
+        Glide.with(context)
+                .load(imageUrl)
+//                .error(R.drawable.flicks_movie_placeholder)
+//                .placeholder(R.drawable.flicks_movie_placeholder)
+                .into(holder.ivPosterImage);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    // creating the Viewholder class
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        // track view objects
+        ImageView ivPosterImage;
+        TextView tvTitle;
+        TextView tvOverview;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ivPosterImage = (ImageView) itemView.findViewById(R.id.ivPosterImage);
+            tvOverview = (TextView) itemView.findViewById(R.id.tvOverview);
+            tvTitle = (TextView) itemView.findViewById(R.id.tvTitle);
+
+        }
+    }
+}
